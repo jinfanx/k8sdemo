4edc749473e84c54fca0820dbe3b50474e5450f7
jinfanx <renjin_fan@foxmail.com>
2020-07-28 00:53:46 +0800

    update clear.sh

--- dockerfiles/servicecomb-service-center/clear.sh
+++ dockerfiles/servicecomb-service-center/clear.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 set +e
-docker ps -a|grep -E "Exited|servicecomb"|awk '{print $1}'|xargs docker stop|xargs docker rm 2>/dev/null
+docker ps -a|grep servicecomb|awk '{print $1}'|xargs docker stop 2>/dev/null
+docker ps -a|grep servicecomb|awk '{print $1}'|xargs docker rm 2>/dev/null
 docker images|grep none|awk '{print $3}'|xargs docker rmi 2>/dev/null
 docker images|grep servicecomb|awk '{print $3}'|xargs docker rmi 2>/dev/null
